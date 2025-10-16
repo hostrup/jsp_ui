e165b51ecc73be1ff7cc8eb80d9427260d6de014
Ronni Hostrup <hostrup@billie>
2025-10-16 07:48:18 +0200

    test fra linux

---
+++ controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("jsp.prototype.controller.App", {
+        onInit: function () {
+            // Controller logic can be added here
+        }
+    });
+});
